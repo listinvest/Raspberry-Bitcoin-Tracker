526706a64facb18ac9ac9aeca2bc186f4245f957
emrearmagan <emre.armagan@hotmail.de>
2019-07-02 22:06:16 +0200

    channel listener

--- main.go
+++ main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-	"Bitcoin-Tracker/client"
-	"errors"
 	"fmt"
 	"github.com/d2r2/go-hd44780"
 	i2c2 "github.com/d2r2/go-i2c"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	//create client and make request
-	c := client.NewClient(30)
+	c := NewClient(30)
 	go c.Get()
 
-	lcd, err := getLCD()
-	if err != nil {
-		c.ErrChan <- err
-	}
-	for {
-		select {
-		case resp := <-c.RespChan: //Handle response here
-			write(lcd, resp.Bpi.USD.Rate)
-		case err := <-c.ErrChan: //Handle error here
-			write(lcd, err.Error())
-		}
-	}
-}
-
-func write(lcd *hd44780.Lcd, message string) {
-	lcd.Home()
-	lcd.SetPosition(0, 0)
-	fmt.Fprint(lcd, message)
-}
-func getLCD() (*hd44780.Lcd, error) {
 	i2c, err := i2c2.NewI2C(0x27, 1)
 	if err != nil {
-		return nil, errors.New("Unable to locate i2c")
+		panic("Unable to locate i2c")
 	}
 	defer i2c.Close()
 
 	lcd, err := hd44780.NewLcd(i2c, hd44780.LCD_16x2)
 	if err != nil {
-		return nil, errors.New("Error with LCD")
+		panic("Error with LCD")
 	}
 
 	lcd.BacklightOn()
 	lcd.Clear()
 
-	return lcd, nil
+	// Go signal notification works by sending `os.Signal`
+	// values on a channel. We'll create a channel to
+	// receive these notifications (we'll also make one to
+	// notify us when the program can exit).
+	sigs := make(chan os.Signal, 1)
+
+	// `signal.Notify` registers the given channel to
+	// receive notifications of the specified signals.
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+loop:
+	for {
+		select {
+		case resp := <-c.RespChan: //Handle response here
+			lcd.Home()
+			lcd.SetPosition(0, 0)
+			fmt.Fprint(lcd, resp.Bpi.USD.Rate)
+		case err := <-c.ErrChan: //Handle error here
+			lcd.Home()
+			lcd.SetPosition(1, 0)
+			fmt.Fprint(lcd, err)
+		case <-sigs:
+			fmt.Println("Got shutdown, exiting")
+			// Break out of the outer for statement and end the program
+			break loop
+		}
+	}
 }
